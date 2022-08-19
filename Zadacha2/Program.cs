d32bff295acc426e67d5b3e86227d7f6b97e944e
Haru <harumolodec@mail.ru>
2022-08-19 12:40:35 +0700

    Zadacha2 END

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,98 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int rows = InputNumbers("Введите количество строк: ");
+int columns = InputNumbers("Введите количество столбцов: ");
+if (rows == columns)
+{
+    Console.WriteLine($"Ты забыл, что такое прямоугольник? Иди гугли");
+    return;
+}
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumLineElements(int[,] matrix, int i)
+{
+    int sumLine = matrix[i, 0];
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        sumLine += matrix[i, j];
+    }
+    return sumLine;
+}
+
+
+int[,] matrix = new int[rows, columns];
+Console.WriteLine($"\nМассив: ");
+Console.WriteLine();
+CreateArray(matrix);
+WriteArray(matrix);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(matrix, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\nСтрока с наименьшей суммой — {minSumLine + 1}. Сумма элементов в строке = {sumLine}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
